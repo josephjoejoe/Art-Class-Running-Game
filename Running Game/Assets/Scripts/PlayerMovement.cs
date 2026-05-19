66c2504597304c9c4eaa6a0a9ce8394ca7b96bac
Joseph <jpraposo2027@gmail.com>
2026-05-19 19:14:39 -0400

    Made simple ground and player movement

---
+++ Running Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public float jumpForce;
+
+    public Rigidbody2D RB;
+
+    //box Cast for ground detection 
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+
+    //Animator anim;
+    //public bool Moving;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newScale = transform.localScale;
+        float currentScale = Mathf.Abs(transform.localScale.x);
+
+        Vector2 vel = new Vector2(0, RB.linearVelocity.y);
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            vel.x = -Speed;
+            newScale.x = currentScale;
+            //Moving = true;
+        }
+
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            vel.x = Speed;
+            newScale.x = -currentScale;
+            //Moving = true;
+        }
+
+        if (Input.GetKey("w") && isGrounded() || Input.GetKey(KeyCode.UpArrow) && isGrounded())
+        {
+            // velocity is a vector 
+            vel.y = jumpForce;
+        }
+
+
+        //if (Input.GetKeyUp("a") || Input.GetKeyUp("d") || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    Moving = false;
+        //}
+
+        RB.linearVelocity = vel;
+
+        //anim.SetBool("Moving", Moving);
+        transform.localScale = newScale;
+    }
+
+    public bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+    }
+
+}
